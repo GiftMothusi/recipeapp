339db05fcc530f0e64f4cf1b22265575f24e8943
ThatoGift <giftgabanakgosi@gmail.com>
2023-06-28 07:58:19 +0200

    nabar+form update

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { MdFoodBank } from "react-icons/md";
+import { IoMdMenu } from "react-icons/io";
+
+import "./Header.scss";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [scrolled, setScrolled] = useState();
+
+  return (
+    <nav
+      className={`navbar bg-orange flex align-center ${
+        scrolled ? "scrolled" : ""
+      }`}
+    >
+      <div className="container w-100">
+        <div className="navbar-content text-white">
+          <div className="brand-and-toggler flex align-center justify-between">
+            <Link to="/" className="navbar-brand fw-3 fs-22 flex align-center">
+              <MdFoodBank />
+              <span className="navbar-brand-text fw-7">Tasty Treats</span>
+            </Link>
+            <div className="navbar-btns flex align-center">
+              <button
+                type="button"
+                className="navbar-show-btn text-white"
+                onClick={{}}
+              >
+                <IoMdMenu size={27} />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/components/Header/SearchForm.js
+++ src/components/Header/SearchForm.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BsSearch } from "react-icons/bs";
+
+import "./Header.scss";
 
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  return (
+    <form className="search-form flex align-center" onSubmit={{}}>
+      <input
+        type="text"
+        className="form-control-input text-dark-gray fs-15"
+        placeholder="find recipes..."
+        onChange={{}}
+      />
+      <button
+        type="submit"
+        className="form-submit-btn text-white text-uppercase fs-14"
+      >
+        <BsSearch className="btn-icon" size={20} />
+      </button>
+    </form>
+  );
 };
 
 export default SearchForm;
